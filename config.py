4e1450e1d58376ddf2326411b2b2493df0174eb5
sreekanth000 <76695849+sreekanth000@users.noreply.github.com>
2025-06-15 11:14:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,10 +10,10 @@ class Config:
 
     # MySQL Database Configuration
     # IMPORTANT: Replace with your actual MySQL credentials
-    DB_HOST = "tafeprivatecallcenter.cjbmyqbvrjhw.ap-south-1.rds.amazonaws.com"
-    DB_USER = "root"  # e.g., 'root'
-    DB_PASSWORD = "MPCTafeMF2024_06_21_KSC"  # e.g., 'mypassword'
-    DB_NAME = "retail_invoice_db"
+    DB_HOST = "xxxxxxxxxxxxxx"
+    DB_USER = "xxxxxxxxxxxx"  # e.g., 'root'
+    DB_PASSWORD = "xxxxxxxxxxxxxxxxx"  # e.g., 'mypassword'
+    DB_NAME = "xxxxxxxxxx"
 
     # Flask Secret Key for session management (IMPORTANT for production)
-    SECRET_KEY = "MPCTafeMF2024_06_21_KSC"  # In production, use a strong, randomly generated key
+    SECRET_KEY = "xxxxxxxxxxxxxxxx"  # In production, use a strong, randomly generated key
